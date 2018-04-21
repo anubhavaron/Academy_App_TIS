8414c82ac96c7c3ff61a965f2b1067bbba89842a
Anubhav <anubhavaron000051@gmail.com>
2018-04-21 23:43:52 +0530

    ADD batches

---
+++ app/src/main/java/com/example/pc/academy_app_tis/head/Background_add_batch.java
@@ -0,0 +1,72 @@
+package com.example.pc.academy_app_tis.head;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by pc on 4/21/2018.
+ */
+
+public class Background_add_batch extends AsyncTask<String,Void,String> {
+    Context context;
+    String url_insert="https://tisabcd12.000webhostapp.com/head/Adding_Batch.php";
+
+    public Background_add_batch(Context context) {
+        this.context=context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String batch_subject=strings[0];
+        String batch_class=strings[1];
+        String batch_number=strings[2];
+
+        try {
+            URL url=new URL(url_insert);
+            HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            OutputStream outputStream=httpURLConnection.getOutputStream();
+
+            BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+
+            String data= URLEncoder.encode("batch_subject","UTF-8")+"="+URLEncoder.encode(batch_subject,"UTF-8")+"&"+
+                    URLEncoder.encode("batch_class","UTF-8")+"="+URLEncoder.encode(batch_class,"UTF-8")+"&"+
+                    URLEncoder.encode("batch_number","UTF-8")+"="+URLEncoder.encode(batch_number,"UTF-8");
+            bufferedWriter.write(data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+            InputStream IS=httpURLConnection.getInputStream();
+            IS.close();
+            return "Added ";
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Toast.makeText(context,s,Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/pc/academy_app_tis/head/Head_Batch.java
+++ app/src/main/java/com/example/pc/academy_app_tis/head/Head_Batch.java
@@ -1,12 +1,30 @@
 package com.example.pc.academy_app_tis.head;
 
+import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.pc.academy_app_tis.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Head_Batch extends AppCompatActivity implements Head_Batch_Adapter.Head_Batch_AdapterOnClickHandler {
     RecyclerView recyclerView;
@@ -14,6 +32,7 @@ public class Head_Batch extends AppCompatActivity implements Head_Batch_Adapter.
     String batch_subject[];
     String batch_class[];
     String batch_number[];
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +42,140 @@ public class Head_Batch extends AppCompatActivity implements Head_Batch_Adapter.
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         adapter=new Head_Batch_Adapter(Head_Batch.this);
-        batch_subject=new String[2];
-        batch_class=new String[2];
-        batch_number=new String[2];
-        batch_subject[0]="Science;";
-        batch_subject[1]="Math";
-        batch_class[0]="11";
-        batch_class[1]="11";
-        batch_number[0]="1";
-        batch_number[1]="1";
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.Floating_3);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder mBuilder=new AlertDialog.Builder(Head_Batch.this);
+                View mView=getLayoutInflater().inflate(R.layout.dialog_batch_adding,null);
+                final EditText sub=(EditText)mView.findViewById(R.id.subject_5);
+                final EditText cl=(EditText)mView.findViewById(R.id.class_5);
+                final EditText n=(EditText)mView.findViewById(R.id.batch_5);
+                final Button add=(Button)mView.findViewById(R.id.add_5);
+                add.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        add.setEnabled(false);
+
+                        Background_add_batch background_task_add_batches=new Background_add_batch(Head_Batch.this);
+
+                        background_task_add_batches.execute(sub.getText().toString(),cl.getText().toString(),n.getText().toString());
+                        Background_batch_details background_batch_details=new Background_batch_details();
+                        background_batch_details.execute();
+
+                    }
+                });
+                mBuilder.setView(mView);
+                AlertDialog dialog=mBuilder.create();
+                dialog.show();
+            }
+        });
+
 
         recyclerView.setAdapter(adapter);
-        adapter.swapCursor(this,batch_subject,batch_class,batch_number);
+        Background_batch_details background_batch_details=new Background_batch_details();
+        background_batch_details.execute();
+
     }
 
     @Override
     public void onClick(int x) {
         Toast.makeText(Head_Batch.this,x+"",Toast.LENGTH_LONG).show();
 
+    }
+
+    class Background_batch_details extends AsyncTask<Void,Void,String>
+    {   String json_url="https://tisabcd12.000webhostapp.com/head/batches_details.php";
+
+        @Override
+        protected void onPreExecute() {
+            //   Toast.makeText(login_signup.this,"Hey",Toast.LENGTH_SHORT).show();
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected void onPostExecute(String JSON_STRING) {
+                JSONObject  jsonObject;
+            JSONArray jsonArray;
+
+
+
+
+            try {
+                jsonObject=new JSONObject(JSON_STRING);
+                int count=0;
+
+
+                jsonArray=jsonObject.getJSONArray("server response");
+                int size=jsonArray.length();
+                batch_subject=new String[size];
+                batch_class=new String[size];
+                batch_number=new String[size];
+                while(count<jsonArray.length())
+                {
+                    JSONObject JO=jsonArray.getJSONObject(count);
+                    batch_subject[count]=JO.getString("batch_subject");
+                    batch_class[count]=JO.getString("batch_class");
+                    batch_number[count]=JO.getString("batch_number");
+
+
+                    count++;
+
+
+                }
+                if(batch_class!=null)
+                {
+                    adapter.swapCursor(getApplicationContext(),batch_subject,batch_class,batch_number);
+                }
+
+
+
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            super.onPostExecute(JSON_STRING);
+        }
+
+
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String json_string;
+            try {
+                URL url=new URL(json_url);
+                HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                InputStream inputStream=httpURLConnection.getInputStream();
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder=new StringBuilder();
+                while((json_string=bufferedReader.readLine())!=null)
+                {
+                    stringBuilder.append(json_string+"\n");
+
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
     }
 }
