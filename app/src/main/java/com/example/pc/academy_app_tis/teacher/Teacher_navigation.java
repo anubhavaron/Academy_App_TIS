ca6d2fea9232becb54a0753b125778e92e55a462
Anubhav <anubhavaron000051@gmail.com>
2018-04-26 19:43:23 +0530

    teacher_batch++

--- app/src/main/java/com/example/pc/academy_app_tis/teacher/Teacher_navigation.java
+++ app/src/main/java/com/example/pc/academy_app_tis/teacher/Teacher_navigation.java
@@ -1,5 +1,6 @@
 package com.example.pc.academy_app_tis.teacher;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,11 +13,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.pc.academy_app_tis.R;
 
 public class Teacher_navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    static String batch_subject;
+    static String batch_class;
+    static String batch_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,41 @@ public class Teacher_navigation extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+
+        batch_subject=getIntent().getStringExtra("batch_subject");
+        batch_class=getIntent().getStringExtra("batch_class");
+        batch_number=getIntent().getStringExtra("batch_number");
+        Toast.makeText(Teacher_navigation.this,batch_subject+batch_class+batch_number,Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     @Override
@@ -84,6 +125,11 @@ public class Teacher_navigation extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+
+            Intent i=new Intent(Teacher_navigation.this,Teachers_students.class);
+            startActivity(i);
+
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
