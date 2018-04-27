7fe32324d33f9ce8d7e11c206ac46a968d0ea73b
Anubhav <anubhavaron000051@gmail.com>
2018-04-27 15:01:43 +0530

    test_re

---
+++ app/src/main/java/com/example/pc/academy_app_tis/teacher/Background_test_record.java
@@ -0,0 +1,83 @@
+package com.example.pc.academy_app_tis.teacher;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by pc on 4/27/2018.
+ */
+
+public class Background_test_record extends AsyncTask<String,Void,String> {
+    Context context;
+    String url_insert="https://tisabcd12.000webhostapp.com/teacher/adding_new_test_record.php";
+
+    public Background_test_record(Context context) {
+        this.context=context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String batch_subject=strings[0];
+        String batch_class=strings[1];
+        String batch_number=strings[2];
+        String test_name=strings[3];
+        String username=strings[4];
+        String marks_obtained=strings[5];
+        String total_marks=strings[6];
+
+
+        try {
+            URL url=new URL(url_insert);
+            HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            OutputStream outputStream=httpURLConnection.getOutputStream();
+
+            BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+
+            String data= URLEncoder.encode("batch_subject","UTF-8")+"="+URLEncoder.encode(batch_subject,"UTF-8")+"&"+
+                    URLEncoder.encode("batch_class","UTF-8")+"="+URLEncoder.encode(batch_class,"UTF-8")+"&"+
+                    URLEncoder.encode("batch_number","UTF-8")+"="+URLEncoder.encode(batch_number,"UTF-8")+"&"+
+                    URLEncoder.encode("test_name","UTF-8")+"="+URLEncoder.encode(test_name,"UTF-8")+"&"+
+                    URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"+
+                    URLEncoder.encode("marks_obtained","UTF-8")+"="+URLEncoder.encode(marks_obtained,"UTF-8")+"&"+
+                    URLEncoder.encode("total_marks","UTF-8")+"="+URLEncoder.encode(total_marks,"UTF-8")
+                    ;
+            bufferedWriter.write(data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+            InputStream IS=httpURLConnection.getInputStream();
+            IS.close();
+
+            return "Added ";
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Toast.makeText(context,s,Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/pc/academy_app_tis/teacher/Test_Record.java
+++ app/src/main/java/com/example/pc/academy_app_tis/teacher/Test_Record.java
@@ -1,21 +1,19 @@
 package com.example.pc.academy_app_tis.teacher;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.pc.academy_app_tis.R;
-import com.example.pc.academy_app_tis.head.Background_add_batch;
-import com.example.pc.academy_app_tis.head.Head_Batch;
 import com.example.pc.academy_app_tis.head.Students_details_adapter;
 import com.example.pc.academy_app_tis.head.head_navigation;
 
@@ -30,37 +28,49 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
-public class Teacher_new_test extends AppCompatActivity implements  new_test_adapter.new_test_adapterOnClickHandler {
-    String test_name[];
-    String test_marks[];
-    new_test_adapter adapter;
-    RecyclerView recyclerView;
+public class Test_Record extends AppCompatActivity {
+    String test_name;
+    String test_marks;
     FloatingActionButton floatingActionButton;
-    Context context;
+    String name[];
+
+    ArrayAdapter<String> arrayList;
+    ArrayList<String> names;
+     AutoCompleteTextView sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teacher_new_test);
-        recyclerView=(RecyclerView)findViewById(R.id.recycler_29);
-        floatingActionButton=(FloatingActionButton)findViewById(R.id.Floating_29);
+        setContentView(R.layout.activity_test__record);
+        test_name=getIntent().getStringExtra("test_name");
+        test_marks=getIntent().getStringExtra("total_marks");
+        Toast.makeText(Test_Record.this,test_name+test_marks,Toast.LENGTH_LONG).show();
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.Floating_32);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final AlertDialog.Builder mBuilder=new AlertDialog.Builder(Teacher_new_test.this);
-                View mView=getLayoutInflater().inflate(R.layout.dialog_new_test,null);
-                final EditText sub=(EditText)mView.findViewById(R.id.test_name_31);
-                final EditText cl=(EditText)mView.findViewById(R.id.marks_31);
 
-                final Button add=(Button)mView.findViewById(R.id.add_31);
+
+                final AlertDialog.Builder mBuilder=new AlertDialog.Builder(Test_Record.this);
+                View mView=getLayoutInflater().inflate(R.layout.dialog_test_record,null);
+                 sub=(AutoCompleteTextView) mView.findViewById(R.id.name_33);
+                final EditText cl=(EditText)mView.findViewById(R.id.marks_33);
+                Background_getting_students_here background_getting_students_here=new Background_getting_students_here();
+                background_getting_students_here.execute();
+
+                final Button add=(Button)mView.findViewById(R.id.add_33);
                 add.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         add.setEnabled(false);
-                        Background_new_test_add background_new_test_add=new Background_new_test_add(Teacher_new_test.this);
-                        background_new_test_add.execute(Teacher_navigation.batch_subject,Teacher_navigation.batch_class,Teacher_navigation.batch_number,sub.getText().toString(),cl.getText().toString());
-                        new Background_test_info().execute();
+
+
+
+                       Background_test_record background_new_test_add=new Background_test_record(Test_Record.this);
+                        background_new_test_add.execute(Teacher_navigation.batch_subject,Teacher_navigation.batch_class,Teacher_navigation.batch_number,test_name,sub.getText().toString(),cl.getText().toString(),test_marks);
+
 
                    /*     Background_add_batch background_task_add_batches=new Background_add_batch(Head_Batch.this);
 
@@ -72,26 +82,16 @@ public class Teacher_new_test extends AppCompatActivity implements  new_test_ada
                 mBuilder.setView(mView);
                 AlertDialog dialog=mBuilder.create();
                 dialog.show();
+
             }
         });
-        context=getApplicationContext();
-        new Background_test_info().execute();
-    }
 
-    @Override
-    public void onClick(int x) {
-
-        Intent intent=new Intent(Teacher_new_test.this,Test_Record.class);
-        intent.putExtra("test_name", test_name[x]);
-        intent.putExtra("total_marks",test_marks[x]);
-
-        startActivity(intent);
 
     }
 
 
-    class Background_test_info extends AsyncTask<Void,Void,String>
-    {   String json_url="https://tisabcd12.000webhostapp.com/teacher/getting_new_test.php?batch_subject="+ Teacher_navigation.batch_subject+"&batch_class="+Teacher_navigation.batch_class+"&batch_number="+Teacher_navigation.batch_number;
+    class Background_getting_students_here extends AsyncTask<Void,Void,String>
+    {   String json_url="https://tisabcd12.000webhostapp.com/head/getting_students.php?batch_subject="+ Teacher_navigation.batch_subject+"&batch_class="+Teacher_navigation.batch_class+"&batch_number="+Teacher_navigation.batch_number;
 
         @Override
         protected void onPreExecute() {
@@ -104,7 +104,7 @@ public class Teacher_new_test extends AppCompatActivity implements  new_test_ada
         protected void onPostExecute(String JSON_STRING) {
             JSONObject jsonObject;
             JSONArray jsonArray;
-            //Toast.makeText(getApplicationContext(),JSON_STRING,Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),JSON_STRING,Toast.LENGTH_LONG).show();
 
 
 
@@ -116,25 +116,23 @@ public class Teacher_new_test extends AppCompatActivity implements  new_test_ada
 
                 jsonArray=jsonObject.getJSONArray("server response");
                 int size=jsonArray.length();
-                test_marks=new String[size];
-
-                test_name=new String[size];
+                name=new String[size];
+                names=new ArrayList<String>();
                 while(count<jsonArray.length())
                 {
                     JSONObject JO=jsonArray.getJSONObject(count);
-                    test_name[count]=JO.getString("test_name");
-                    test_marks[count]=JO.getString("total_marks");
+                    name[count]=JO.getString("username");
+                    names.add(name[count]);
 
                     count++;
 
 
                 }
-                LinearLayoutManager layoutManager=new LinearLayoutManager(Teacher_new_test.this);
-                recyclerView.setLayoutManager(layoutManager);
-                recyclerView.setHasFixedSize(true);
-                adapter=new new_test_adapter(Teacher_new_test.this);
-                recyclerView.setAdapter(adapter);
-                adapter.swapCursor(context,test_name,test_marks);
+                arrayList = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, names);
+                arrayList.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                sub.setAdapter(arrayList);
+
+
 
 
 
